024a693811bb3d7d0734f3a12a3eccd6c4562cf7
briankarani67 <briankarani884@gmail.com>
2026-06-17 06:28:02 +0300

    Jobee

--- Backend/app.py
+++ Backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -15,8 +15,15 @@ def get(name):
 
 @app.route("/add/<int:num1>/<int:num2>")
 def add(num1, num2):
-    added = num1+num2
-    return f"{num1} + {num2} = {num1+num2}"
+    added = f"{num1} + {num2} = {num1+num2}"
+    return added
+
+@app.route('/handle_params')
+def handle():
+    name = request.args['name']
+    age = request.args['age']
+    return f"My name is {name}, am {age} years old."
+
 
 
 if __name__ == '__main__':
